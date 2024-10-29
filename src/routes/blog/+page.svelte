7f6ac6dfa25094173074b98f68021ba437a39f74
erik-dickens <121179746+erik-dickens@users.noreply.github.com>
2024-10-29 15:27:30 +0100

    Add blog functionality
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/erik-dickens/test-svelte?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
+  import posts from './posts.json';
+
+  let blogPosts = [];
+
+  onMount(() => {
+    blogPosts = posts;
+  });
+</script>
+
+<style>
+  @import './blog.css';
+</style>
+
+<main>
+  <h1>Blog</h1>
+  <ul>
+    {#each blogPosts as post}
+      <li>
+        <a href={`/blog/${post.slug}`}>{post.title}</a>
+      </li>
+    {/each}
+  </ul>
+</main>
